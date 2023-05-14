66f8ff9f810329442b9e12f0f1825ad2f3f92309
nikita-skobov <nikita@skobov.io>
2023-05-14 15:11:18 -0600

    starts aws cfn generation project

---
+++ downloader/src/lib.rs
@@ -0,0 +1,81 @@
+use std::io::{Read, Cursor};
+
+use ureq;
+
+pub use zip::ZipArchive;
+
+pub type Zipped = ZipArchive<Cursor<Vec<u8>>>;
+
+fn basic_hash(data: &[u8]) -> u32 {
+    adler32::adler32(data).unwrap_or(0)
+}
+
+/// given a valid region, we form a url like:
+/// https://schema.cloudformation.{region}.amazonaws.com/CloudformationSchema.zip
+/// and fetch and return the zip file as a vec of bytes.
+/// optionally, if cache is set to true, we will:
+/// - try to read the file from cache if it exists, if not: make url request.
+/// - if we made a url request, we save the file to disk.
+pub fn get_cfn_resource_provider_schema(
+    region: &str,
+    cache: bool,
+) -> Result<Zipped, String> {
+    let url = format!("https://schema.cloudformation.{region}.amazonaws.com/CloudformationSchema.zip");
+    let url_hash = basic_hash(url.as_bytes());
+    let file_name = format!("{url_hash}.zip");
+    if cache {
+        match std::fs::File::open(&file_name) {
+            Ok(mut f) => {
+                // if we're able to read it then we're good.
+                // but if we fail, then we'll try to make a request.
+                let mut bytes = vec![];
+                if let Ok(_) = f.read_to_end(&mut bytes) {
+                    return Ok(bytes_to_zip(bytes)?);
+                }
+            }
+            Err(_) => {
+                // we assume it doesnt exist, so we just make a request instead.
+            }
+        }
+    }
+    // either cache is false, or we failed to read the file.
+    // in any case: download it.
+    let contents = download_file(&url)?;
+    // if cache is set, we'd like to save this file
+    if cache {
+        let _ = std::fs::write(&file_name, &contents);
+    }
+    bytes_to_zip(contents)
+}
+
+pub fn download_file(
+    url: &str,
+) -> Result<Vec<u8>, String> {
+    let body = ureq::get(url)
+        .call().map_err(|e| format!("Failed to make http request to {}\n{:?}", url, e))?;
+    let mut data_reader = body.into_reader();
+    let mut data = vec![];
+    data_reader.read_to_end(&mut data)
+        .map_err(|e| format!("Failed to read request bytes of {}\n{:?}", url, e))?;
+    Ok(data)
+}
+
+pub fn bytes_to_zip(
+    bytes: Vec<u8>,
+) -> Result<Zipped, String> {
+    let reader = std::io::Cursor::new(bytes);
+    let archive = zip::ZipArchive::new(reader)
+        .map_err(|e| format!("Failed to create zip archive\n{:?}", e))?;
+    Ok(archive)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn can_download() {
+        let contents = get_cfn_resource_provider_schema("us-east-2", true).expect("Failed");
+        // bytes_to_zip(contents).expect("Failed to make zip");
+    }
+}
---
+++ parser/src/lib.rs
@@ -0,0 +1,274 @@
+use std::{collections::HashMap, io::Read};
+
+use downloader::{get_cfn_resource_provider_schema, Zipped};
+
+use serde::{Deserialize};
+
+/// the schema of the data that cloudformation provides from these files:
+/// https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/resource-type-schemas.html
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+#[serde(deny_unknown_fields)]
+#[serde(default)]
+#[derive(Default)]
+pub struct CfnResourceProviderSchema {
+    // simple metadata stuff:
+    pub type_name: String,
+    pub description: String,
+    pub additional_properties: bool,
+    pub taggable: bool,
+    #[serde(rename = "$comment")]
+    pub comment: String,
+    #[serde(rename = "$schema")]
+    pub schema: String,
+    #[serde(rename = "type")]
+    pub ty: String,
+    pub title: String,
+
+    // meat and potatoes:
+    pub definitions: HashMap<String, Property>,
+    pub properties: HashMap<String, Property>,
+
+    // extra complex metadata. defines which fields are
+    // inputs/outputs, permissions, etc.
+    pub required: Vec<String>,
+    pub all_of: Vec<Requirement>,
+    pub any_of: Vec<Requirement>,
+    pub one_of: Vec<Requirement>,
+    pub handlers: Handlers,
+    pub create_only_properties: Vec<String>,
+    pub primary_identifier: Vec<String>,
+    pub additional_identifiers: Vec<Vec<String>>,
+    pub read_only_properties: Vec<String>,
+    pub write_only_properties: Vec<String>,
+    pub deprecated_properties: Vec<String>,
+    pub conditional_create_only_properties: Vec<String>,
+    pub non_public_properties: Vec<String>,
+    pub non_public_definitions: Vec<String>,
+    pub tagging: Tagging,
+    pub replacement_strategy: String,
+
+    // stuff i dont care about:
+    pub source_url: String,
+    pub resource_link: serde_json::Value,
+    pub documentation_url: String,
+    pub remote: serde_json::Value,
+    pub property_transform: serde_json::Value,
+    pub type_configuration: serde_json::Value,
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+#[serde(deny_unknown_fields)]
+#[serde(default)]
+#[derive(Default)]
+pub struct Property {
+    pub insertion_order: bool,
+    pub array_type: ArrayType,
+    pub title: String,
+    pub description: String,
+    pub examples: serde_json::Value,
+    pub default: DefaultType,
+    pub multiple_of: f64,
+    pub maximum: f64,
+    pub exclusive_maximum: f64,
+    pub minimum: f64,
+    pub exclusive_minimum: f64,
+    pub max_length: usize,
+    pub min_length: usize,
+    pub pattern: String,
+    pub max_items: usize,
+    pub min_items: usize,
+    pub unique_items: bool,
+    pub required: Vec<String>,
+    pub all_of: Vec<Property>,
+    pub any_of: Vec<Property>,
+    pub one_of: Vec<Property>,
+    pub format: String,
+    pub contains: Vec<String>,
+    pub max_properties: usize,
+    pub min_properties: usize,
+    pub properties: HashMap<String, Property>,
+    pub additional_properties: bool,
+    pub dependencies: HashMap<String, serde_json::Value>,
+    #[serde(rename = "type")]
+    pub ty: PropertyType,
+    #[serde(rename = "enum")]
+    pub enm: PropertyType,
+    #[serde(rename = "const")]
+    pub cnst: String,
+
+    // i dont care:
+    pub items: serde_json::Value,
+    #[serde(rename = "$ref")]
+    pub r: String,
+    #[serde(rename = "$comment")]
+    pub comment: String,
+    pub pattern_properties: serde_json::Value,
+}
+
+#[derive(Deserialize)]
+#[serde(deny_unknown_fields)]
+pub enum ArrayType {
+    Standard,
+    AttributeList,
+}
+
+impl Default for ArrayType {
+    fn default() -> Self {
+        ArrayType::Standard
+    }
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+#[serde(deny_unknown_fields)]
+pub enum Requirement {
+    Required(Vec<String>),
+    AllOf(Vec<Requirement>),
+    OneOf(Vec<Requirement>),
+    AnyOf(Vec<Requirement>),
+}
+
+impl Default for Requirement {
+    fn default() -> Self {
+        Self::Required(vec![])
+    }
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+#[serde(deny_unknown_fields)]
+#[serde(default)]
+#[derive(Default)]
+pub struct Handlers {
+    pub create: HandlerDef,
+    pub read: HandlerDef,
+    pub update: HandlerDef,
+    pub delete: HandlerDef,
+    pub list: HandlerDef,
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+#[serde(deny_unknown_fields)]
+#[serde(default)]
+#[derive(Default)]
+pub struct HandlerDef {
+    pub permissions: Vec<String>,
+    pub timeout_in_minutes: u32,
+    // i dont care:
+    pub handler_schema: serde_json::Value,
+}
+
+#[derive(Deserialize)]
+#[serde(deny_unknown_fields)]
+#[serde(untagged)]
+pub enum PropertyType {
+    String(String),
+    VecStrings(Vec<String>),
+    VecNums(Vec<f64>),
+}
+impl Default for PropertyType {
+    fn default() -> Self {
+        Self::String("".to_string())
+    }
+}
+
+#[derive(Deserialize)]
+#[serde(deny_unknown_fields)]
+#[serde(untagged)]
+pub enum DefaultType {
+    Bool(bool),
+    String(String),
+    Num(f64),
+    Map(HashMap<String, DefaultType>),
+    Null(Option<()>),
+    List(Vec<DefaultType>),
+}
+impl Default for DefaultType {
+    fn default() -> Self {
+        Self::Null(None)
+    }
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+#[serde(deny_unknown_fields)]
+#[serde(default)]
+#[derive(Default)]
+pub struct Tagging {
+    pub taggable: bool,
+    pub tag_on_create: bool,
+    pub tag_updatable: bool,
+    pub cloud_formation_system_tags: bool,
+    pub tag_property: String,
+}
+
+pub fn iterate_all_zip_files(mut contents: Zipped, cb: &mut impl FnMut(&str, String)) {
+    let mut filenames = contents.file_names().map(|x| x.to_string()).collect::<Vec<String>>();
+    filenames.sort();
+    for filename in filenames.iter() {
+        match contents.by_name(&filename) {
+            Ok(mut file) => {
+                let mut s = String::new();
+                if let Ok(_) = file.read_to_string(&mut s) {
+                    cb(filename, s);
+                }
+            }
+            Err(e) => {
+                panic!("REEEE\n{:?}", e);
+            }
+        }
+        // TODO: what if we fail?
+    }
+}
+
+pub fn get_schema(name: &str, data: &String) -> CfnResourceProviderSchema {
+    match serde_json::from_str::<CfnResourceProviderSchema>(data) {
+        Err(e) => {
+            panic!("Failed to deserialize {name}\n{:?}", e);
+        }
+        Ok(o) => o,
+    }
+}
+
+pub fn get_service_name(name: &str, schema: &CfnResourceProviderSchema) -> String {
+    let mut segments = schema.type_name.split("::");
+    let _ = segments.next()
+        .unwrap_or_else(|| {
+            panic!("Failed to get first type name segment of {} From {}", schema.type_name, name);
+        });
+    // assume first == "AWS"
+    let second = segments.next()
+        .unwrap_or_else(|| {
+            panic!("Failed to get second type name segment of {} From {}", schema.type_name, name);
+        });
+    second.to_string()
+}
+
+pub fn sort_by_service(name: &str, schema: CfnResourceProviderSchema, map: &mut HashMap<String, Vec<CfnResourceProviderSchema>>) {
+    let name = get_service_name(name, &schema);
+    if let Some(existing) = map.get_mut(&name) {
+        existing.push(schema);
+    } else {
+        map.insert(name.clone(), vec![schema]);
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn all_files_deser_correctly() {
+        let zip_archive = get_cfn_resource_provider_schema("us-east-2", true).expect("Failed to");
+        let mut map = HashMap::new();
+        iterate_all_zip_files(zip_archive, &mut |name, data| {
+            let schema = get_schema(name, &data);
+            sort_by_service(name, schema, &mut map);
+        });
+        println!("{:#?}", map.keys());
+    }
+}
