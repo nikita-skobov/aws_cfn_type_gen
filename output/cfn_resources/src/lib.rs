ee13f5a8d7247e87a81b7820cc57486992b4804f
nikita-skobov <nikita@skobov.io>
2023-05-21 10:43:51 -0600

    validate fn returns Err(str)

--- output/cfn_resources/src/lib.rs
+++ output/cfn_resources/src/lib.rs
@@ -4,24 +4,21 @@ pub trait CfnResource {
 
     fn properties(self) -> serde_json::Value;
 
-    /// returns Some(string) if there is a validation error.
-    fn validate(&self) -> Option<String> {
-        None
+    /// returns Err(string) if there is a validation error.
+    fn validate(&self) -> Result<(), String> {
+        Ok(())
     }
 
     /// validate first by the default validation function,
     /// and then add optional extra check via passing your own external validation function.
-    fn validate_extern(&self, cb: fn(&Self) -> Option<String> ) -> Option<String> {
-        let default_validation = self.validate();
-        if default_validation.is_some() {
-            return default_validation;
-        }
+    fn validate_extern(&self, cb: fn(&Self) -> Result<(), String> ) -> Result<(), String> {
+        self.validate()?;
         cb(self)
     }
 
     /// like validate_extern, but does not use the default validation. This method
     /// only validates via your custom validation function.
-    fn validate_override(&self, cb: fn(&Self) -> Option<String> ) -> Option<String> {
+    fn validate_override(&self, cb: fn(&Self) -> Result<(), String> ) -> Result<(), String> {
         cb(self)
     }
 }
