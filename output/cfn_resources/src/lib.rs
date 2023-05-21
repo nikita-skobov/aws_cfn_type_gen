74f2490a11bd7ac67a47395b58c0cddd9ccd243f
nikita-skobov <nikita@skobov.io>
2023-05-21 14:17:15 -0600

    makes cfn resourec type safe, super trait to be able to serialize

--- output/cfn_resources/src/lib.rs
+++ output/cfn_resources/src/lib.rs
@@ -1,8 +1,6 @@
-pub trait CfnResource {
+pub trait CfnResource: serde::Serialize {
     /// returns a string like 'AWS::CloudFront::Distribution'
     fn type_string(&self) -> &'static str;
-
-    fn properties(&self) -> serde_json::Value;
 
     /// returns Err(string) if there is a validation error.
     fn validate(&self) -> Result<(), String> {
@@ -11,14 +9,18 @@ pub trait CfnResource {
 
     /// validate first by the default validation function,
     /// and then add optional extra check via passing your own external validation function.
-    fn validate_extern(&self, cb: fn(&Self) -> Result<(), String> ) -> Result<(), String> {
+    fn validate_extern(&self, cb: fn(&Self) -> Result<(), String> ) -> Result<(), String>
+        where Self: Sized
+    {
         self.validate()?;
         cb(self)
     }
 
     /// like validate_extern, but does not use the default validation. This method
     /// only validates via your custom validation function.
-    fn validate_override(&self, cb: fn(&Self) -> Result<(), String> ) -> Result<(), String> {
+    fn validate_override(&self, cb: fn(&Self) -> Result<(), String> ) -> Result<(), String>
+        where Self: Sized
+    {
         cb(self)
     }
 }
